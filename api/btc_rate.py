eb4fca59a7e9d80ba91c1e544ed418880510df5c
mahmoodoffice9-code <mahmoodoffice9@gmail.com>
2026-09-12 02:43:01 +0500

    Update btc_rate.py

--- api/btc_rate.py
+++ api/btc_rate.py
@@ -1,6 +1,6 @@
-from http.server import BaseHTTPRequestHandler
 import json
 import requests
+from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -8,20 +8,20 @@ class handler(BaseHTTPRequestHandler):
             url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
             response = requests.get(url)
             data = response.json()
-
+            
             btc_price = data.get("price", "N/A")
-
+            
             result = {
                 "status": "success",
                 "coin": "Bitcoin (BTC)",
                 "price_usd": btc_price
             }
-
+            
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps(result).encode('utf-8'))
-
+            
         except Exception as e:
             self.send_response(500)
             self.send_header('Content-type', 'application/json')
